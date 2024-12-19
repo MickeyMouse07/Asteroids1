8288f9bd5240c5fa14ac8c46fbd199bed113b612
Chananet <chananet7@gmail.com>
2024-12-19 18:23:29 +1100

    add screen

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pygame
 import constants
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+ #   print("Starting asteroids!")
+ #   print(f"Screen width: {constants.SCREEN_WIDTH}")
+ #   print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 
 
