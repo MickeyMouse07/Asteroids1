ff64c84912c2531a23858a7717a68641993b25b3
Chananet <chananet7@gmail.com>
2024-12-28 16:32:49 +1100

    add player

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 import constants
+from player import Player
+
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player =  Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -13,6 +18,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        
+        player.draw(screen)    
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
