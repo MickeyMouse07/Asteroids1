0f8df760c3804f04537c4f2b7893ff7db28e4fe0
Chananet <chananet7@gmail.com>
2024-12-31 18:42:35 +1100

    add containers

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player =  Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group() 
+    Player.containers = (updatable, drawable)
+    Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    
+
     dt = 0
+
 
     while True:
         screen.fill((0, 0, 0))
@@ -19,8 +25,15 @@ def main():
                 return
             
         
-        player.draw(screen)
-        player.update(dt)
+    
+
+        for draw in drawable:
+            draw.draw(screen)
+
+        for update in updatable:
+            update.update(dt)
+
+        
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
