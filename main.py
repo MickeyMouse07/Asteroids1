e4ec3d17153e649f425e74d80da14897fa60e3f0
Chananet <chananet7@gmail.com>
2025-01-03 19:54:15 +1100

    asteriod

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import constants
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 
@@ -9,9 +11,13 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group() 
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    AsteroidField()
     
 
     dt = 0
@@ -23,23 +29,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        
-    
+                 
+ 
+        for update in updatable:
+            update.update(dt)
+
 
         for draw in drawable:
             draw.draw(screen)
 
-        for update in updatable:
-            update.update(dt)
+
+        
 
         
         pygame.display.flip()
         dt = clock.tick(60)/1000
-
- #   print("Starting asteroids!")
- #   print(f"Screen width: {constants.SCREEN_WIDTH}")
- #   print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 
 
