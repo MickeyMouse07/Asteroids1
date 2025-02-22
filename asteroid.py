6aa063830fa5fd1e2d76dd23d710ef98f2e8d580
Chananet <chananet7@gmail.com>
2025-02-22 19:32:01 +1100

    split

--- asteroid.py
+++ asteroid.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 from circleshape import CircleShape
 
+import random
+
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
@@ -15,3 +17,12 @@ class Asteroid(CircleShape):
 
     def split(self):
         self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        randomAngle = random.uniform(20,50)
+        smallerA = Asteroid(self.position.x, self.position.y, (self.radius - ASTEROID_MIN_RADIUS ))
+        smallerB = Asteroid(self.position.x, self.position.y, (self.radius - ASTEROID_MIN_RADIUS ))
+        smallerA.velocity = pygame.math.Vector2.rotate(self.velocity, randomAngle) * 1.2
+        smallerB.velocity = pygame.math.Vector2.rotate(self.velocity, -randomAngle) * 1.2
